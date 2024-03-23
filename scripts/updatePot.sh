3a91aa1370253988b14ec428813e82d2923e9a44
David Harder <david@davidjharder.ca>
2024-03-22 21:46:02 -0500

    Initial wrapping for translations

--- scripts/updatePot.sh
+++ scripts/updatePot.sh
@@ -22,9 +22,13 @@ for file in `find src -name "*.ui"`; do
     fi
 done
 
-# NOT .ui or ,build files, which should leave the python
-for file in `find src -not  -name "*.ui" -or -name "*.build"`; do
+# The python files
+for file in `find src -name "*.py"` ; do
         do_gettext $file --add-comments
 done
+
+# Apparently, --from-code=UTF-8 is ignored if all your strings are ascii?
+sed -i 's/charset=CHARSET/charset=UTF-8/g' solus-mate-transition-tool.po
+
 mv solus-mate-transition-tool.po po/solus-mate-transition-tool.pot
 #tx push -s
